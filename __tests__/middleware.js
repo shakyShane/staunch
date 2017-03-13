7ad1acce568278db9d6e5aba6231f02128403b51
Shane Osbourne <shane.osbourne8@gmail.com>
2017-03-13 21:43:33 +0000

    middleware chaining test

--- __tests__/middleware.js
+++ __tests__/middleware.js
@@ -42,4 +42,35 @@ describe('supporting middleware', function () {
 
         expect(myMock.mock.calls.length).toEqual(1);
     });
+    it('supports middleware at store creation via chaining', function () {
+
+        const myMock = jest.fn();
+
+        const store = createStore()
+            .addMiddleware([
+                function (store) {
+                    store.action$.subscribe(myMock);
+                }
+            ]);
+
+        store.register({
+            state: {
+                user: {
+                    name: 'shane'
+                }
+            },
+            reducers: [
+                function (user, action) {
+                    switch(action.type) {
+                        case 'USER_MODDED':
+                            return user.set('name', action.payload)
+                    }
+                    return user;
+                }
+            ]
+        });
+
+        store.dispatch({type: 'USER_CREATE', payload: {name: 'shane'}});
+        expect(myMock.mock.calls.length).toEqual(2);
+    });
 });
