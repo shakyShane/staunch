857a60c0bbcaf9e8728c686dd73c7cccbd67c405
Shane Osbourne <shane.osbourne8@gmail.com>
2017-04-18 12:27:29 +0100

    Class constructor for store

---
+++ src/StaunchStore.ts
@@ -0,0 +1,244 @@
+import {alwaysArray, alwaysMap, getMap} from "./index";
+import {Map} from 'immutable';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Subject} from 'rxjs/Subject';
+import {assignFn, concatFn} from "./subjects";
+import {Observable, Subscription} from "rxjs";
+import {gatherEffects} from "./addEffects";
+import {gatherReducers, InputTypes} from "./addReducers";
+
+export interface IStoreProps {
+    state?: any
+    reducers?: any[]
+    effects?: any[]
+    middleware?: Function[]
+    extras?: any
+}
+
+export interface IAction {
+    type: string
+    payload?: any
+    via?: any
+    error?: boolean
+    meta?: any
+}
+
+export interface IActionWithState {
+    action: IAction
+    state: Map<string, any>
+}
+
+export class StaunchStore {
+    public action$: Subject<IAction>;
+    public newExtras$: Subject<any>;
+    public userExtra$: Subject<any>;
+    public newResponses: Subject<any>;
+    public newReducer$: Subject<any>;
+    public newMappedReducer$: Subject<any>;
+    public storeReducers: BehaviorSubject<any>;
+    public state$: BehaviorSubject<Map<string, any>>;
+    public mappedReducers: BehaviorSubject<any>;
+    public storeResponses: BehaviorSubject<any>;
+    public subs: Subscription[];
+    public actionsWithState$: Observable<IActionWithState>;
+    public actionsWithResultingStateUpdate$: Observable<IActionWithState>;
+
+    isOpen = true;
+
+    constructor(props: IStoreProps) {
+
+        this.state$ = new BehaviorSubject(props.state);
+        this.subs   = [];
+
+        this.userExtra$ = new BehaviorSubject({});
+        this.newExtras$ = new Subject();
+        this.subs.push(this.newExtras$.scan(assignFn, {}).subscribe(this.userExtra$));
+
+        // reducers to act upon state
+        this.storeReducers = new BehaviorSubject([]);
+        this.newReducer$ = new Subject();
+        this.subs.push(this.newReducer$.scan(concatFn, []).subscribe(this.storeReducers));
+
+        // Mapped reducers
+        this.mappedReducers = new BehaviorSubject([]);
+        this.newMappedReducer$ = new Subject();
+        this.subs.push(this.newMappedReducer$.scan(concatFn, []).subscribe(this.mappedReducers));
+
+        // responses
+        this.storeResponses = new BehaviorSubject([]);
+        this.newResponses = new Subject();
+        this.subs.push(this.newResponses.scan(concatFn, []).subscribe(this.storeResponses));
+
+        // stream of actions
+        this.action$ = new Subject();
+
+        this.actionsWithState$ = this.action$.withLatestFrom(this.state$, function (action, state) {
+            return {
+                action,
+                state
+            }
+        });
+        this.actionsWithResultingStateUpdate$ = this.actionsWithState$;
+    }
+
+    register(input) {
+        const {state, reducers, effects, responses} = input;
+
+        if (state) {
+            this._registerOnStateTree(state);
+        }
+
+        if (reducers) {
+            this._addReducers(reducers);
+        }
+
+        if (effects) {
+            this._addEffects(effects);
+        }
+
+        if (responses) {
+            this._addResponses(responses);
+        }
+
+        return this;
+    }
+
+    private _registerOnStateTree(state) {
+        for (let key in state) {
+            // now init with action
+            this.dispatcher({
+                type: '@@NS-INIT('+ key +')',
+                payload: {
+                    path: [key],
+                    value: state[key]
+                }
+            });
+        }
+    }
+
+    private _addExtras(extras) {
+        alwaysArray(extras).forEach((extra) => {
+            this.newExtras$.next(extra);
+        });
+    }
+
+    private _addResponses (responses) {
+        alwaysArray(responses).forEach((resp) => {
+            Object.keys(resp).forEach((actionName) => {
+                const item = resp[actionName];
+                this.newResponses.next({
+                    name: actionName,
+                    path: [].concat(item.path).filter(Boolean),
+                    targetName: item.action
+                });
+            });
+        });
+    }
+    /**
+     * Dispatch 1 or many actions
+     * @param action
+     * @returns {*}
+     * @private
+     */
+    public dispatcher(action) {
+        if (!this.isOpen) {
+            return;
+        }
+        if (Array.isArray(action)) {
+            return action.forEach((a) => {
+                this.action$.next(a)
+            });
+        }
+        return this.action$.next(action);
+    }
+
+    private _addMiddleware(middleware) {
+        alwaysArray(middleware).forEach((middleware) => {
+            middleware.call(null, this);
+        })
+    }
+
+    private _addEffects(incoming) {
+        /**
+         * Default extras that get passed to all 'effects'
+         */
+        const storeExtras = {
+            state$: this.state$,
+            action$: this.action$,
+            actionsWithState$: this.actionsWithState$,
+            actionsWithResultingStateUpdate$: this.actionsWithState$
+        };
+
+        gatherEffects(incoming, this.actionsWithState$, storeExtras, this.userExtra$)
+            .forEach(outgoing => {
+                if (outgoing.type === InputTypes.Effect) {
+                    this.subs.push(outgoing.payload.subscribe(this.dispatcher.bind(this)));
+                }
+            });
+    }
+
+    private _addReducers(incoming) {
+        gatherReducers(incoming)
+            .forEach(outgoing => {
+                if (outgoing.type === InputTypes.Reducer) {
+                    this.newReducer$.next(outgoing.payload);
+                }
+                if (outgoing.type === InputTypes.MappedReducer) {
+                    this.newMappedReducer$.next(outgoing.payload);
+                }
+                if (outgoing.type === InputTypes.State) {
+                    this._registerOnStateTree(outgoing.payload);
+                }
+            })
+    }
+    public addReducers (reducers) {
+        this._addReducers(reducers);
+        return this;
+    }
+    public dispatch (action: IAction) {
+        this.dispatcher(action);
+        return this;
+    }
+    public getState(path: string|string[]) {
+        const lookup = alwaysArray(path);
+        return this.state$.getValue().getIn(lookup, getMap({}));
+    }
+    public toJS(path: string|string[]) {
+        const lookup = alwaysArray(path);
+        return this.state$.getValue().getIn(lookup, getMap({})).toJS();
+    }
+    public toJSON(path: string|string[]) {
+        const lookup = alwaysArray(path);
+        return this.state$.getValue().getIn(lookup, getMap({})).toJSON();
+    }
+    public addMiddleware(middleware) {
+        this._addMiddleware(middleware);
+        return this;
+    }
+    public changes(path) {
+        const lookup = alwaysArray(path);
+        return this.state$.map(x => x.getIn(lookup))
+            .distinctUntilChanged();
+    }
+    public once(actions: string|string[]) {
+        const lookup = alwaysArray(actions);
+        return this.actionsWithState$.filter((x: {action: any}) => {
+            return lookup.indexOf(x.action.type) > -1;
+        }).take(1);
+    }
+    public addExtras(extras) {
+        this._addExtras(extras);
+        return this;
+    }
+    public addEffects(effects) {
+        this._addEffects(effects);
+        return this;
+    }
+    public close() {
+        if (this.isOpen) {
+            this.subs.forEach(sub => sub.unsubscribe());
+            this.isOpen = false;
+        }
+        return this;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ import Immutable = require('immutable');
 import {Observable} from 'rxjs/Observable';
 import {actionStream} from "./actions";
 import {handleResponses} from "./responses";
-import {gatherReducers, InputTypes} from "./addReducers";
-import {gatherEffects} from "./addEffects";
-import {concatFn, assignFn} from "./subjects";
-
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {Subject} from 'rxjs/Subject';
 
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/do';
@@ -20,236 +14,38 @@ import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/observable/of';
+import {StaunchStore, IStoreProps} from "./StaunchStore";
 
 export enum ReducerTypes {
     MappedReducer = <any>'MappedReducer',
     GlobalReducer = <any>'GlobalReducer'
 }
 
-export function createStore(initialState,
-                            initialReducers,
-                            initialEffects,
-                            initialMiddleware,
-                            initialExtras) {
+export function createStore(props: IStoreProps = {}): StaunchStore {
 
-    const mergedInitialState = alwaysMap(initialState);
-    const state$             = new BehaviorSubject(mergedInitialState);
+    const mergedInitialState = alwaysMap(props.state);
+    const store = new StaunchStore({...props, state: mergedInitialState});
+
     const subs = [];
 
-    const userExtra$ = new BehaviorSubject({});
-    const newExtras$ = new Subject();
-    subs.push(newExtras$.scan(assignFn, {}).subscribe(userExtra$));
-
-    // reducers to act upon state
-    const storeReducers = new BehaviorSubject([]);
-    const newReducer$ = new Subject();
-    subs.push(newReducer$.scan(concatFn, []).subscribe(storeReducers));
-
-    // Mapped reducers
-    const mappedReducers = new BehaviorSubject([]);
-    const newMappedReducer$ = new Subject();
-    subs.push(newMappedReducer$.scan(concatFn, []).subscribe(mappedReducers));
-
-    // responses
-    const storeResponses = new BehaviorSubject([]);
-    const newResponses = new Subject();
-    subs.push(newResponses.scan(concatFn, []).subscribe(storeResponses));
-
-    // stream of actions
-    const action$ = new Subject();
-
     // stream
-    subs.push(actionStream(mergedInitialState, action$, storeReducers, mappedReducers)
-        .subscribe(state$));
-
-    /**
-     * Create a stream that has updates + resulting state updates
-     */
-    const actionsWithState$ = action$.withLatestFrom(state$, function (action, state) {
-        return {
-            action,
-            state
-        }
-    });
+    subs.push(
+        actionStream(mergedInitialState, store.action$, store.storeReducers, store.mappedReducers)
+            .subscribe(store.state$)
+    );
 
     /**
      * Setup responses for declarative cross-domain communication
      */
-    subs.push(handleResponses(actionsWithState$, storeResponses)
-        .subscribe(action => _dispatcher(action)));
+    subs.push(handleResponses(store.actionsWithState$, store.storeResponses)
+        .subscribe(action => store.dispatcher(action)));
 
-    /**
-     * Default extras that get passed to all 'effects'
-     */
-    const storeExtras = {
-        state$,
-        action$,
-        actionsWithState$,
-        actionsWithResultingStateUpdate$: actionsWithState$
-    };
+    store.addReducers(props.reducers);
+    store.addEffects(props.effects);
+    store.addMiddleware(props.middleware);
+    store.addExtras(props.extras);
 
-
-    /**
-     * Dispatch 1 or many actions
-     * @param action
-     * @returns {*}
-     * @private
-     */
-    function _dispatcher(action) {
-        if (Array.isArray(action)) {
-            return action.forEach(function(a) {
-                action$.next(a)
-            });
-        }
-        return action$.next(action);
-    }
-
-
-
-    function _addMiddleware(middleware) {
-        alwaysArray(middleware).forEach(function (middleware) {
-            middleware.call(null, api);
-        })
-    }
-
-    function _addExtras(extras) {
-        alwaysArray(extras).forEach(function (extra) {
-            newExtras$.next(extra);
-        });
-    }
-
-    function _registerOnStateTree(state) {
-        for (let key in state) {
-            // now init with action
-            _dispatcher({
-                type: '@@NS-INIT('+ key +')',
-                payload: {
-                    path: [key],
-                    value: state[key]
-                }
-            });
-        }
-    }
-
-    function _addResponses (responses) {
-        alwaysArray(responses).forEach(function (resp) {
-            Object.keys(resp).forEach(function (actionName) {
-                const item = resp[actionName];
-                newResponses.next({
-                    name: actionName,
-                    path: [].concat(item.path).filter(Boolean),
-                    targetName: item.action
-                });
-            });
-        });
-    }
-
-    function _addEffects(incoming) {
-        gatherEffects(incoming, actionsWithState$, storeExtras, userExtra$)
-            .forEach(outgoing => {
-                if (outgoing.type === InputTypes.Effect) {
-                    subs.push(outgoing.payload.subscribe(_dispatcher));
-                }
-            });
-    }
-
-    function _addReducers(incoming) {
-        gatherReducers(incoming)
-            .forEach(outgoing => {
-                if (outgoing.type === InputTypes.Reducer) {
-                    newReducer$.next(outgoing.payload);
-                }
-                if (outgoing.type === InputTypes.MappedReducer) {
-                    newMappedReducer$.next(outgoing.payload);
-                }
-                if (outgoing.type === InputTypes.State) {
-                    _registerOnStateTree(outgoing.payload);
-                }
-            })
-    }
-
-    const api = {
-        isOpen: true,
-        state$: state$,
-        action$: action$,
-        actionsWithState$: actionsWithState$,
-        actionsWithResultingStateUpdate$: actionsWithState$,
-        register: function (input) {
-            const {state, reducers, effects, responses} = input;
-
-            if (state) {
-                _registerOnStateTree(state);
-            }
-
-            if (reducers) {
-                _addReducers(reducers);
-            }
-
-            if (effects) {
-                _addEffects(effects);
-            }
-
-            if (responses) {
-                _addResponses(responses);
-            }
-
-            return api;
-        },
-        addReducers: function (reducers) {
-            _addReducers(reducers);
-            return api;
-        },
-        dispatch: function (action) {
-            _dispatcher(action);
-            return api;
-        },
-        getState: function (path) {
-            const lookup = alwaysArray(path);
-            return state$.getValue().getIn(lookup, getMap({}));
-        },
-        toJS: function (path) {
-            const lookup = alwaysArray(path);
-            return state$.getValue().getIn(lookup, getMap({})).toJS();
-        },
-        toJSON: function (path) {
-            const lookup = alwaysArray(path);
-            return state$.getValue().getIn(lookup, getMap({})).toJSON();
-        },
-        addMiddleware: function (middleware) {
-            _addMiddleware(middleware);
-            return api;
-        },
-        once: function (actions) {
-            const lookup = alwaysArray(actions);
-            return actionsWithState$.filter((x: {action: any}) => {
-                return lookup.indexOf(x.action.type) > -1;
-            }).take(1);
-        },
-        changes: function (path) {
-            const lookup = alwaysArray(path);
-            return state$.map(x => x.getIn(lookup))
-                .distinctUntilChanged();
-        },
-        addExtras: function(extras) {
-            _addExtras(extras);
-            return api;
-        },
-        close: function() {
-            if (api.isOpen) {
-                subs.forEach(sub => sub.unsubscribe());
-                api.isOpen = false;
-            }
-            return api;
-        }
-    };
-
-    // add initial ones
-    _addReducers(initialReducers);
-    _addEffects(initialEffects);
-    _addMiddleware(initialMiddleware);
-    _addExtras(initialExtras);
-
-    return api;
+    return store;
 }
 
 export function alwaysArray (input) {
